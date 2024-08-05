762bf5efc94360ac695b4c8f7360a7a8deeb6a4f
Chris Zietlow <aczietlow@gmail.com>
2024-08-05 07:15:51 -0400

    Slapped the forms within an accordion.

---
+++ web/js/accordion.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const headers = document.querySelectorAll('.accordion-header');
+
+    headers.forEach(header => {
+        header.addEventListener('click', function() {
+            const content = this.nextElementSibling;
+
+            if (content.classList.contains('expanded')) {
+                content.classList.remove('expanded')
+            } else {
+                // If we only want a single item open at a time.
+                document.querySelectorAll('.expanded').forEach(item => {
+                    item.classList.remove('expanded')
+                });
+                content.classList.add('expanded')
+            }
+        });
+    });
+});
